a3c32cef2994e81fb3b61e6581b565d700e74b77
marcelodoqa <hideo.toma@icloud.com>
2023-05-09 18:10:48 -0300

    inserindo integracao continua, end

--- READM.md
+++
@@ -1,3 +0,0 @@
-git push # Testes _end-to-end_ com Cypress
-
-TBD.
--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'va759k',
   //outras configurações...
 
   e2e: {
